895a79f25bfa08a1da29811f8ab195f37c28e569
Eldar Khaitov <Eldar_Khaitov@epam.com>
2016-07-15 16:13:15 +0300

    Element added

--- src/main.js
+++ src/main.js
@@ -1,43 +1,72 @@
-var name = 'nastya';
-var CommentForm = React.createClass({
-    render: function() {
-        return (
-            <div className="commentForm">
-                Hello, world! I am a CommentForm.
-            </div>
-        );
+class Element extends React.Component {
+    render() {
+        return <div className="slickbox" {...this.props}></div>;
     }
-});
-var CommentList = React.createClass({
-    render: function() {
-        return (
-            <div className="commentList">
-                Hello, world! I am a CommentList. {this.props.name}
-            </div>
-        );
-    }
-});
-var CommentBox = React.createClass({
-    render: function() {
-        return (
-            <div className="commentBox">
-                <h1>Comments</h1>
-                <CommentList name={name}/>
-                <CommentForm />
-            </div>
-        );
-    }
-});
-setInterval(() => {
-    name = new Date().getTime();
-    ReactDOM.setState({name: name});
-}, 2000);
-setTimeout(Render, 1000);
-
-function Render() {
-    ReactDOM.render(
-        <CommentBox />,
-        document.getElementById('content')
-    );
-    // console.log('render called');
 }
+
+class Buttons extends React.Component {
+    render() {
+        return <div>
+            <button id="show">Show all</button>
+            <button id="hide">Hide all</button>
+        </div>;
+    }
+}
+
+class Component extends React.Component{
+    constructor(props) {
+        super(props);
+        this.handleElementClick = this.handleElementClick.bind(this);
+        this.handleButtonClick = this.handleButtonClick.bind(this);
+        this.state = {};
+        this.reinitElements(true)
+    }
+
+    reinitElements(shouldShow, index) {
+        if(index === undefined) {
+            this.indents[index] = <Element onClick={this.handleElementClick} data-show={false} data-index={index} key={index}>Element {index + 1}</Element>;
+            return;
+        }
+        this.indents = [];
+        for (var i = 0; i < 8; i++) {
+            this.indents.push(<Element onClick={this.handleElementClick} data-show={shouldShow} data-index={i} key={i}>Element {i + 1}</Element>);
+        }
+    }
+
+    handleElementClick(e) {
+        var self = this;
+        function display() {
+            var index = +e.target.getAttribute('data-index');
+            self.reinitElements(false, index)
+        }
+        self.setState({
+            display: display()
+        });
+    }
+
+    handleButtonClick(e) {
+        var self = this;
+        function display() {
+            if(e.target.id === 'show') {
+                self.reinitElements(true);
+            } else if(e.target.id === 'hide') {
+                self.reinitElements(false)
+            }
+        }
+        self.setState({
+            display: display()
+        });
+    }
+
+    render() {
+        return <div>
+            <div onClick={this.handleButtonClick}>
+                <Buttons />
+            </div>
+            {this.indents.map( (el, index) => {
+            return el.props['data-show'] ? el : null;
+        })} </div>
+    }
+}
+
+ReactDOM.render(<Component />, document.getElementById('container'));
